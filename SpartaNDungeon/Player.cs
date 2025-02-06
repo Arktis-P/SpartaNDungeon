e49ecaed7369691a9b7b7fb5eb4eb6af776e74bf
Arktis-P <71116951+Arktis-P@users.noreply.github.com>
2025-02-06 18:59:26 +0900

    [feat] player 기본 기능 추가
    - 직업에 따라 추가 스탯 부여
    - 레벨업/레벨업 체크 메서드 추가
    - 죽음/죽음 체크 메서드 추가

--- SpartaNDungeon/Player.cs
+++ SpartaNDungeon/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,40 +14,68 @@ namespace SpartaNDungeon
         // name, job, level  // atk, def, luk, dex  // hp, mp  // gold
         public string Name { get; set; }
         public string Job { get; }
-        public int Level { get; }
-        public int Attack { get; }
-        public int Defense { get; }
-        public int Intelligence { get; }
-        public int Luck { get; }
-        public int Dexterity { get; }
+        public int Level { get; private set; }
+        public int Attack { get; private set; }
+        public int Defense { get; private set; }
+        public int Intelligence { get; private set; }
+        public int Luck { get; private set; }
+        public int Dexterity { get; private set; }
         public int Health { get; }
         public int Mana { get; }
         public int Gold { get; }
-        public int Exp { get; }
+        public int Exp { get; private set; }
 
         // intermediate stats
         // max health  // level exp 
         public int MaxHealth { get; }
         public int MaxMana { get; }
-        public int LevelExp { get; }
+        public int LevelExp { get; private set; }
 
         // complex stats  
         // inventory
-        //public List<Item> inventory;
+        public List<Item> inventory;
 
         // player class initiate
         public Player(string name, int jobId)
         {
             Name = name; Level = 1; Job = Enum.GetName(typeof(JobType), jobId);
-            Attack = 10; Defense = 10; Intelligence = 10; Luck = 10; Dexterity = 10;
+            Attack = 5; Defense = 5; Intelligence = 5; Luck = 5; Dexterity = 5;
             Health = 100; Mana = 100;
             Gold = 1000; Exp = 0;
 
             MaxHealth = 100;  // may change dynamically with player's other stats (ex. level, attack, etc.)
             MaxMana = 100;  // may change dynamically with player's other stats (ex. level, intelligence, etc.)
-            LevelExp = 100;  // may change dynamically with player's level
+            LevelExp = 100 * Level;  // requied exp increases as level increases
 
-            //inventory = new List<Item>();
+            // player's inventoy list
+            // write after item class is made
+
+            // give player additional stat according to its job
+            AddStatus(Job);
+        }
+
+        // job gives additional stats
+        private void AddStatus(string job)
+        {
+            JobType jobType;
+            JobType.TryParse(job, out jobType);
+
+            switch (jobType)
+            {
+                case JobType.전사:  // warrior
+                    Attack += 5; Defense += 5;
+                    break;
+                case JobType.마법사:  // magician
+                    Attack += 5; Intelligence += 5;
+                    break;
+                case JobType.도적:  // logue
+                    Luck += 5; Dexterity += 5;
+                    break;
+                case JobType.궁수:  // archer
+                    Attack += 5; Dexterity += 5;
+                    break;
+                default: break;  // default, no additional stats
+            }
         }
 
         // display player's status
@@ -63,8 +92,54 @@ namespace SpartaNDungeon
 
         public void DisplayInventory()
         {
-
+            return;
         }
-        
+
+        // check if player can level up
+        public void CheckLevelUp()
+        {
+            if (Exp >= LevelExp) { Levelup(); }
+        }
+        // increase player's level and initiate related values (exp, level exp)
+        private void Levelup()
+        {
+            // increase level
+            Level++;
+            // initiate exp to 0
+            Exp = 0;
+            // set new level exp
+            LevelExp = 100 * Level;
+        }
+
+        // check if player is dead
+        public void CheckDead()
+        {
+            if (Health <= 0) { PlayerDead(); }
+        }
+        // features done when player is dead
+        private void PlayerDead()
+        {
+            // when player is dead
+            // show death msg
+            Console.WriteLine();
+            Console.WriteLine($"{Name}이(가) 죽었습니다.");
+            // ask if retry
+            // [later] insert on UI class
+            Console.WriteLine();
+            Console.WriteLine("다시 시작하시겠습니까?");
+            Console.WriteLine("1. 예");
+            Console.WriteLine("2. 아니오");
+
+            // get player's input
+            int input;
+            int.TryParse(Console.ReadLine(), out input);
+            switch (input)
+            {
+                case 1:  // to generation page
+                    break;
+                case 2:  // to endgame page
+                    break;
+            }
+        }
     }
 }

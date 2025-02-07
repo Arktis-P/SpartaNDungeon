7b232fe9f70916c86a3d3bd924265d280791199c
JUNG99 <kinggodjung9@naver.com>
2025-02-07 10:55:01 +0900

    [feat] 아이템 구매/판매 추가
    - Buy/Sell Item으로 샀을 때는 Gold감소 팔았을 때는 Gold추가
    
    -아이템을 사고 팔면 인벤토리 업데이트

--- SpartaNDungeon/Shop.cs
+++ SpartaNDungeon/Shop.cs
@@ -28,12 +28,35 @@ namespace SpartaNDungeon
 
         public void DisplayItem(Item item)
         {
-            Console.WriteLine("상점:");
+            Console.WriteLine("--- 상점 --- ");
             foreach (var item in ItemSale)
             {
-                Console.WriteLine($"{item.Name}(itme.Price) gold");
+                Console.WriteLine($"{item.Name}({item.Price}) gold");
             }
         }
+        
+        public void BuyItem(Player player, Item item)
+        {
+            if(player.Gold >= selectedItem.Price)
+            {
+                player.Gold -= selectedItem.Price;
+                Console.WriteLine($"{selectedItem.Name} 구매완료!");
+                player.Inventory.Add(selectedItem); //구매한 아이템 인벤토리로
+            }
+
+            else
+            {
+                Console.WriteLine("골드가 충분하지 않습니다.");
+            }
+        }
+        public void SellItem(Player player, int Item item)
+        {
+            int sellPrce = Item.Price / 2;
+            player.Gold += sellPrce;
+            Console.WriteLine($"{Item.Name}이/가 {sellPrce}gold 로 판매되었습니다!");
+            player.DisplayInventory().Remove(Item); //판매한 아이템 인벤토리에서 제거
+        }
+            
     }
 
 }

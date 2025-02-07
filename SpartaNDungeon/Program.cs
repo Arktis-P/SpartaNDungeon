3f4034cd5d1d269994d51be8eeb8f9ff65acc0cc
Arktis-P <71116951+Arktis-P@users.noreply.github.com>
2025-02-07 11:37:17 +0900

    [feat] Program.cs 추가
    - 게임 시작을 위한 정적 클래스 Program 추가.
    - 게임 시작을 위한 Game 클래스 추가

---
+++ SpartaNDungeon/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpartaNDungeon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            game.Start();
+        }
+    }
+
+    class Game
+    {
+        private Player player;
+
+        public void Start()
+        {
+            ShowIntroduction();
+            InitializePlayer();
+            UI.StartPage();
+        }
+    }
+}

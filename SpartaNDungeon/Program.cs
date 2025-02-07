824821d0dfb626c3361d6ab33f3665e62edeec9d
Arktis-P <71116951+Arktis-P@users.noreply.github.com>
2025-02-07 13:02:55 +0900

    [feat] 시작 화면 생성
    - 게임을 시작했을 때 소개 문구 페이지 생성
    - 캐릭터 생성 과정 (이름 및 직업 선택) 추가
    - 전체적인 인터페이스 수정

--- SpartaNDungeon/Program.cs
+++ SpartaNDungeon/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,12 +19,12 @@ namespace SpartaNDungeon
     class Game
     {
         private Player player;
+        private UI ui;
 
         public void Start()
         {
-            ShowIntroduction();
-            InitializePlayer();
-            UI.StartPage();
+            ui.IntroductionPage();
+            ui.StartPage();
         }
     }
 }
--- SpartaNDungeon/UI.cs
+++ SpartaNDungeon/UI.cs
@@ -8,12 +8,52 @@ namespace SpartaNDungeon
 {
     public class UI
     {
-        Player player = new Player();
-        Item item = new Item();
-        Dungeon dungeon = new Dungeon();
+        Player player;
+        
+        public void IntroductionPage()  // 게임 시작 시 소개 화면
+        {
+            Console.Clear();
+            Console.WriteLine("이곳은 스파르타 던전으로 향하는 길입니다.");
+            Console.WriteLine("이곳을 지나가기 위해서는 당신에 대한 정보가 필요합니다.");
+            GeneratePage();
+        }
+        private void GeneratePage()  // 캐릭터 생성 화면
+        {
+            string name;
+            int jobId;
+            Console.WriteLine();
+            // get player name
+            Console.WriteLine("당신의 이름을 알려주세요.");
+            name = Console.ReadLine();
+            // get player job
+            while (true)
+            {
+                Console.WriteLine("당신의 직업을 알려주세요.");
+                Console.WriteLine();
+                Console.WriteLine("1. 전사\n2. 법사\n3. 도적\n4. 궁수");
+                // get player input
+                if(int.TryParse(Console.ReadLine(), out int input) && input >= 1 && input <= 4)
+                {
+                    jobId = input;
+                    InitializePlayer(name, jobId);
+                }
+                else { Console.WriteLine("잘못된 입력입니다."); break; }
+            }
+        }
+
+        private void InitializePlayer(string name, int jobId)
+        {
+            // instantiate player
+            Player player = new Player(name, jobId);
+
+            // show welcome 
+            Console.Clear();
+            Console.WriteLine($"스파르타 던전 입구 마을에 오신 것을 환영합니다, {player.Name} 님!");
+        }
 
         public void StartPage() // 메인 화면
         {
+            Console.Clear();
             Console.WriteLine("스파르타 던전에 오신 여러분 환영합니다.");
             Console.WriteLine();
             Console.WriteLine("1. 상태보기");
@@ -28,25 +68,120 @@ namespace SpartaNDungeon
             switch(input)
             {
                 case 1:
-                    player.DisplayStatus(); 
+                    StatusPage();  // to status page
                     break;
                 case 2:
-                    player.DisplayInventory();
+                    InventoryPage();  // to inventory page
                     break;
                 case 3:
-                    item.DisplayItem();
+                    ShopPage();  // to shop page
                     break;
                 case 4:
-                    dungeon.EnterDungeon();
+                    DungeonPage();  // to dungeon page
                     break;
                 case 5:
+                    QuestPage();  // to quest page
                     break;
                 case 6:
+                    SavePage();  // to save page
                     break;
             }
-
         }
 
+        // status page
+        private void StatusPage()
+        {
+            while (true)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("\t\t==== 상태보기 ====");
+                Console.WriteLine("캐릭터의 현재 상태를 확인할 수 있습니다.");
 
+                Console.WriteLine("");
+                player.DisplayStatus();
+                // 0. 나가기
+            }
+        }
+        // inventory page
+        private void InventoryPage()
+        {
+            while (true)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("\t\t==== 인벤토리 ====");
+                Console.WriteLine("캐릭터의 인벤토리를 확인하고, 장비를 관리할 수 있습니다.");
+
+                // 1. 장착관리
+                // 0. 나가기
+            }
+        }
+        // shop page
+        private void ShopPage()
+        {
+            while (true)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("\t\t==== 상점 ====");
+                Console.WriteLine("필요한 아이템을 구매하고 필요 없는 아이템을 판매할 수 있습니다.");
+
+                // 1. 구매하기
+                // 2. 판매하기
+                // 3. 나가기
+            }
+        }
+        // dungeon page
+        private void DungeonPage()
+        {
+            while (true)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("\t\t==== 던전입장 ====");
+                Console.WriteLine("던전에 입장할 수 있습니다.");
+
+                // 1. 전투시작
+                // 2. 아이템 사용
+                // 3. 나가기
+            }
+        }
+        // quest page
+        private void QuestPage()
+        {
+            while (true)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("\t\t==== 퀘스트 ====");
+                Console.WriteLine("퀘스트 기능은 현재 구현되지 않았습니다.");
+                // show option
+                Console.WriteLine("");
+                // 1~99. (퀘스트 목록 확인)
+                // 0. 나가기
+                Console.WriteLine("0. 나가기");
+
+                // get player's input
+                int input = ConsoleUtil.GetInput(0, 0);
+                // back to startpage
+                StartPage(); break;
+            }
+        }
+        // save page
+        private void SavePage()
+        {
+            while (true)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("\t\t==== 저장하기 ====");
+                Console.WriteLine("저장하기 기능은 현재 구현되지 않았습니다.");
+                // show option
+                Console.WriteLine("");
+                // 1. 저장하기
+                // 0. 나가기
+                Console.WriteLine("0. 나가기");
+
+                // get player's input
+                int input = ConsoleUtil.GetInput(0, 0);
+                // back to startpage
+                StartPage(); break;
+            }
+        }
     }
 }

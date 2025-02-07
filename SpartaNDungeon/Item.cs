da576fb1617b484a985215e01f8a884df7398195
hee103 <gmlgml3213@gmail.com>
2025-02-07 10:42:23 +0900

    [FEAT] 회복물약 보유량 표시
    Item 생성자 Count 추가
    DisplayItem() 보유량 출력 로직 구현
    UseItem() 과 Count에 대한 고민 필요

--- SpartaNDungeon/Item.cs
+++ SpartaNDungeon/Item.cs
@@ -1,4 +1,6 @@
-﻿namespace SpartaNDungeon
+﻿using System.Numerics;
+
+namespace SpartaNDungeon
 {
     internal class Program
     {
@@ -12,13 +14,13 @@
 
         static List<Item> itemList = new List<Item>
             {
-                new Item("수련자의 갑옷", ItemType.Armor, 4, "수련에 도움을 주는 갑옷입니다.", 1000),
-                new Item("무쇠갑옷", ItemType.Armor, 9, "무쇠로 만들어져 튼튼한 갑옷입니다.", 2000),
-                new Item("스파르타의 갑옷", ItemType.Armor, 15, "스파르타의 전사들이 사용했다는 전설의 갑옷입니다.", 3500),
-                new Item("낡은 검", ItemType.Weapon, 5, "쉽게 볼 수 있는 낡은 검입니다.", 600),
-                new Item("청동 도끼", ItemType.Weapon, 10, "어디선가 사용됐던 것 같은 도끼입니다.", 1500),
-                new Item("스파르타의 창", ItemType.Weapon, 15, "스파르타의 전사들이 사용했다는 전설의 창입니다.", 2500),
-                new Item("회복 물약", ItemType.Potion, 30, "체력을 30 회복 할 수 있습니다.", 1000)
+                new Item("수련자의 갑옷", ItemType.Armor, 4, "수련에 도움을 주는 갑옷입니다.", 1000,0),
+                new Item("무쇠갑옷", ItemType.Armor, 9, "무쇠로 만들어져 튼튼한 갑옷입니다.", 2000,0),
+                new Item("스파르타의 갑옷", ItemType.Armor, 15, "스파르타의 전사들이 사용했다는 전설의 갑옷입니다.", 3500,0),
+                new Item("낡은 검", ItemType.Weapon, 5, "쉽게 볼 수 있는 낡은 검입니다.", 600,0),
+                new Item("청동 도끼", ItemType.Weapon, 10, "어디선가 사용됐던 것 같은 도끼입니다.", 1500,0),
+                new Item("스파르타의 창", ItemType.Weapon, 15, "스파르타의 전사들이 사용했다는 전설의 창입니다.", 2500,0),
+                new Item("회복 물약", ItemType.Potion, 30, "체력을 30 회복 할 수 있습니다.", 1000,3)
             };
 
         public class Item
@@ -28,16 +30,18 @@
             public int Value { get; }
             public string Descrip { get; }
             public int Cost { get; }
+            public int Count { get; set; }
             public bool IsPurchase { get; set; }
             public bool IsEquip { get; set; }
 
-            public Item(string name, ItemType type, int value, string descrip, int cost)
+            public Item(string name, ItemType type, int value, string descrip, int cost, int count)
             {
                 Name = name;
                 Type = type;
                 Value = value;
                 Descrip = descrip;
                 Cost = cost;
+                Count = count < 0 ? 0 : count; ;
                 IsPurchase = false;
                 IsEquip = false;
             }
@@ -45,7 +49,16 @@
             public string DisplayItem()
             {
                 string str = IsEquip ? "[E] " : "";
-                str += $"{Name} | {GetType()} | {Descrip} | {GetPriceString()}";
+                str += $"- {Name} | {GetType()} | {Descrip} | {GetPriceString()}";
+                if (Type == ItemType.Potion)
+                {
+                    str += $"  (보유량: {Count})";
+                    if(IsEquip==true)
+                    {
+                        Count--;
+
+                    }
+                }
                 return str;
             }
 
@@ -68,7 +81,21 @@
 
             public int UseItem()
             {
-                // 아이템 사용 로직 추가 가능
+                // 아이템 사용 로직 추가 
+                //if (Type == ItemType.Potion)
+                //{
+                //    if (Count > 0)
+                //    {
+                //        Count--;
+                //        return player.Health+=30; // 사용 성공
+                //    }
+                //    else
+                //    {
+                //        Console.WriteLine("포션이 부족합니다!");
+                //        return player.Health; // 사용 실패
+                //    }
+                //}
+
                 return 0;
             }
         }

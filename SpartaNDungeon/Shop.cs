8010ac72065f8bfe179557103ca0e7ed1a83ce0c
JUNG99 <kinggodjung9@naver.com>
2025-02-07 10:26:28 +0900

    [feat] Shop.cs 추가
    -상점의 인벤토리 출력전에 "상점:" 제목 먼저 콘솔에 출력
    
    - foreach로 ItemSale 리스트에 있는 아이템
    
    - DiplayItem에서 아이템 이름과 가격을 출력

---
+++ SpartaNDungeon/Shop.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpartaNDungeon
+{
+    internal class Shop
+    {
+        public List<Item> ItemSale { get; set; }
+        
+        public Shop()
+        {
+            ItemSale = new List<Item>();
+
+        }
+
+        public void AddItem(Item item)
+        {
+            ItemSale.Add(item);
+
+        }
+        public void RemoveItem(Item item)
+        {
+            ItemSale.Remove(item);
+        }
+
+        public void DisplayItem(Item item)
+        {
+            Console.WriteLine("상점:");
+            foreach (var item in ItemSale)
+            {
+                Console.WriteLine($"{item.Name}(itme.Price) gold");
+            }
+        }
+    }
+
+}

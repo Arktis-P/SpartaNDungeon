ec0cdf30332acbc88c1b699ae317fb669600c3e6
Pgt103 <ddh06095@gmail.com>
2025-02-10 15:38:55 +0900

    [feat] 텍스트 컬러 변경 메서드와 로딩 메서드 추가

--- SpartaNDungeon/ConsoleUtil.cs
+++ SpartaNDungeon/ConsoleUtil.cs
@@ -20,5 +20,25 @@ namespace SpartaNDungeon
                 Console.WriteLine("잘못된 입력입니다.");
             }
         }
+
+        public static void ColorWrite(string str, ConsoleColor color) // 텍스트 칼라 변경
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(str);
+            Console.ResetColor();
+        }
+
+        public static void Loading() // 로딩
+        {
+            Console.Clear();
+            Console.Write("Loading");
+            string str = ".";
+
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(80);
+                Console.Write(str);
+            }
+        }
     }
 }
